f5cfaa5bae68ab82dc4a79458077e1e6a2399c2a
JulioCesarAlarconUrazan <jcalarcon0990@gmail.com>
2021-07-27 23:25:49 -0500

    Operadores de asignacion 12072021

---
+++ main.js
@@ -0,0 +1,50 @@
+//let nombre = `Julio Cesar Alarcon`;
+// console.log(nombre);
+
+// let x;
+// let y = prompt(`Ingrese su edad`);
+// x = parseInt(y); usamos parseInt para transformar un string a int*/
+// console.log(x, typeof(x));
+
+// Operador de asignacion
+// x = y; Aca asignamos a x el valor de y
+
+// Operacion de adicion
+// let x = 5;
+// let y = 1;
+// console.log(`Valor x inicializada ${x}`);
+// console.log(`Valor y inicializada ${y}`);
+// x += y;
+// x = x + y;
+// console.log(`Valor x operador de adicion ${x}`);
+// console.log(`Valor y operador de asicion ${y}`);
+
+// Operador de sustraccion - NaN significa q no es un numero
+// let x = `10`;
+// let y = 5;
+// x-=y;
+// console.log(x, typeof(x));
+
+// Operador de multiplicacion
+// let x = 9;
+// let y = 8;
+// x *= y;
+// console.log(x, typeof(x));
+
+// Operadores de division
+// let x = 100;
+// let y = 2;
+// x /= y;
+// console.log(x, typeof(x));
+
+// Operador de resto o modulo
+// let x = 15;
+// let y = 3;
+// x %= y;
+// console.log(x, typeof(x));
+
+// Operador de exponenciacion
+let x = '5';
+let y = '2';
+x **= y;
+console.log(x, typeof(x));
